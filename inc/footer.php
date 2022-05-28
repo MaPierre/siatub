e706a7694422ddb896df95b42391e40d2de562ef
MaPierre <irakozepierre2@gmail.com>
2022-05-28 23:14:20 +0200

    other

--- inc/footer.php
+++ inc/footer.php
@@ -1,12 +1,19 @@
-<footer class="main-footer text-sm">
-  <div class="container">
+
+
+
+
+<footer class="bg-light text-center text-lg-start">
+  <div class="col-sm-6 col-md-4 col-lg-12">
         <strong>Tous droits réservés &copy; <?php echo date('Y') ?>. 
         <!-- <a href=""></a> -->
         </strong>
-       <div class="float-right d-none d-sm-inline-block">
+      </div>
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+  <div class="text-center p-3">
           <b><?php echo $_settings->info('short_name') ?> Par: <a href="mailto:irakozepierre2@gmail.com" target="blank">Pierre M</a> </b>
         </div>
-      </div>
-      </footer>
-    </div>
+  </div>
+  <!-- Copyright -->
+</footer>
   
