2557f00616996256b7afd155bd59555a9e790c2e
MaPierre <irakozepierre2@gmail.com>
2022-06-14 12:03:03 +0200

    autre dépot

--- inc/footer.php
+++ inc/footer.php
@@ -1,82 +1,85 @@
 <script>
-  $(document).ready(function(){
-    $('.list-group').each(function(){
-      if(String($(this).text()).trim() == ""){
-        $(this).html("")
-      }
+$(document).ready(function() {
+    $('.list-group').each(function() {
+        if (String($(this).text()).trim() == "") {
+            $(this).html("")
+        }
     })
-    
-     window.viewer_modal = function($src = ''){
-      start_loader()
-      var t = $src.split('.')
-      t = t[1]
-      if(t =='mp4'){
-        var view = $("<video src='"+$src+"' controls autoplay></video>")
-      }else{
-        var view = $("<img src='"+$src+"' />")
-      }
-      $('#viewer_modal .modal-content video,#viewer_modal .modal-content img').remove()
-      $('#viewer_modal .modal-content').append(view)
-      $('#viewer_modal').modal({
-              show:true,
-              backdrop:'static',
-              keyboard:false,
-              focus:true
-            })
-            end_loader()  
 
-  }
-    window.uni_modal = function($title = '' , $url='',$size=""){
+    window.viewer_modal = function($src = '') {
+        start_loader()
+        var t = $src.split('.')
+        t = t[1]
+        if (t == 'mp4') {
+            var view = $("<video src='" + $src + "' controls autoplay></video>")
+        } else {
+            var view = $("<img src='" + $src + "' />")
+        }
+        $('#viewer_modal .modal-content video,#viewer_modal .modal-content img').remove()
+        $('#viewer_modal .modal-content').append(view)
+        $('#viewer_modal').modal({
+            show: true,
+            backdrop: 'static',
+            keyboard: false,
+            focus: true
+        })
+        end_loader()
+
+    }
+    window.uni_modal = function($title = '', $url = '', $size = "") {
         start_loader()
         $.ajax({
-            url:$url,
-            error:err=>{
+            url: $url,
+            error: err => {
                 console.log()
                 alert("An error occured")
             },
-            success:function(resp){
-                if(resp){
+            success: function(resp) {
+                if (resp) {
                     $('#uni_modal .modal-title').html($title)
                     $('#uni_modal .modal-body').html(resp)
-                    if($size != ''){
-                        $('#uni_modal .modal-dialog').addClass($size+'  modal-dialog-centered')
-                    }else{
-                        $('#uni_modal .modal-dialog').removeAttr("class").addClass("modal-dialog modal-md modal-dialog-centered")
+                    if ($size != '') {
+                        $('#uni_modal .modal-dialog').addClass($size +
+                            '  modal-dialog-centered')
+                    } else {
+                        $('#uni_modal .modal-dialog').removeAttr("class").addClass(
+                            "modal-dialog modal-md modal-dialog-centered")
                     }
                     $('#uni_modal').modal({
-                      show:true,
-                      backdrop:'static',
-                      keyboard:false,
-                      focus:true
+                        show: true,
+                        backdrop: 'static',
+                        keyboard: false,
+                        focus: true
                     })
                     end_loader()
                 }
             }
         })
     }
-    window._conf = function($msg='',$func='',$params = []){
-       $('#confirm_modal #confirm').attr('onclick',$func+"("+$params.join(',')+")")
-       $('#confirm_modal .modal-body').html($msg)
-       $('#confirm_modal').modal('show')
+    window._conf = function($msg = '', $func = '', $params = []) {
+        $('#confirm_modal #confirm').attr('onclick', $func + "(" + $params.join(',') + ")")
+        $('#confirm_modal .modal-body').html($msg)
+        $('#confirm_modal').modal('show')
     }
-  })
+})
 </script>
 <footer class="main-footer text-sm">
-  <div class="container">
-        <strong>Tous droits réservés &copy; <?php echo date('Y') ?>. 
-        <!-- <a href=""></a> -->
+    <div class="container">
+        <strong>Tous droits réservés &copy; <?php echo date('Y') ?>.
+            <!-- <a href=""></a> -->
         </strong>
-       <div class="float-right d-none d-sm-inline-block">
-          <b><?php echo $_settings->info('short_name') ?> Par: <a href="mailto:irakozepierre2@gmail.com" target="blank">Pierre M</a> </b>
+        <div class="float-right d-none d-sm-inline-block">
+            <b><?php echo $_settings->info('short_name') ?> Par: <a href="mailto:irakozepierre2@gmail.com"
+                    target="blank">Pierre M</a> </b>
         </div>
-      </div>
-      </footer>
     </div>
-    <!-- ./wrapper -->
+</footer>
+</div>
+<!-- ./wrapper -->
 <div id="libraries">
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
-      $.widget.bridge('uibutton', $.ui.button)
+    $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
     <script src="<?php echo base_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
@@ -106,32 +109,11 @@
     <!-- <script src="<?php echo base_url ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
     <!-- AdminLTE App -->
     <script src="<?php echo base_url ?>dist/js/adminlte.js"></script>
-  </div>   
-    <div class="daterangepicker ltr show-ranges opensright">
-      <div class="ranges">
-        <ul>
-          <li data-range-key="Today">Aujourd'hui</li>
-          <li data-range-key="Yesterday">Hier</li>
-          <li data-range-key="Last 7 Days">7 derniers jours</li>
-          <li data-range-key="Last 30 Days">30 derniers jours</li>
-          <li data-range-key="This Month">Ce Mois</li>
-          <li data-range-key="Last Month">Dernier Mois</li>
-          <li data-range-key="Custom Range">Intervalle personnalisée</li>
-        </ul>
-      </div>
-      <div class="drp-calendar left">
-        <div class="calendar-table"></div>
-        <div class="calendar-time" style="display: none;"></div>
-      </div>
-      <div class="drp-calendar right">
-        <div class="calendar-table"></div>
-        <div class="calendar-time" style="display: none;"></div>
-      </div>
-      <div class="drp-buttons"><span class="drp-selected"></span><button class="cancelBtn btn btn-sm btn-default" type="button">Annuler</button><button class="applyBtn btn btn-sm btn-primary" disabled="disabled" type="button">Appliquer</button> </div>
-    </div>
-    <div class="jqvmap-label" style="display: none; left: 1093.83px; top: 394.361px;">SATUB</div>
+</div>
+<div class="jqvmap-label" style="display: none; left: 1093.83px; top: 394.361px;">SATUB</div>
 <script>
-  $(function(){
-    $('.wrapper>.content-wrapper').css("min-height",$(window).height() - $('#top-Nav').height() - $('#login-nav').height() - $("footer.main-footer").height())
-  })
+$(function() {
+    $('.wrapper>.content-wrapper').css("min-height", $(window).height() - $('#top-Nav').height() - $(
+        '#login-nav').height() - $("footer.main-footer").height())
+})
 </script>
--- login.php
+++ login.php
@@ -1,166 +1,186 @@
 <?php require_once('./config.php') ?>
 <!DOCTYPE html>
 <html lang="fr" class="" style="height: auto;">
+
 <head>
- <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta charset="UTF-8">
-<meta name="author" content="Pierre MANIRAKOZE">
-  <style>
-a:link, a:visited {
-  color: white;
-  padding: 14px 25px;
-  text-align: center;
-  text-decoration: none;
-}
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="UTF-8">
+    <meta name="author" content="Pierre MANIRAKOZE">
+    <style>
+    a:link,
+    a:visited {
+        color: white;
+        padding: 14px 25px;
+        text-align: center;
+        text-decoration: none;
+    }
 
 
-    html, body{
-      height:calc(100%) !important;
-      width:calc(100%) !important;
+    html,
+    body {
+        height: calc(100%) !important;
+        width: calc(100%) !important;
     }
-    body{
-      background-image: url("<?php echo validate_image($_settings->info('cover')) ?>");
-      background-size:cover;
-      background-repeat:no-repeat;
+
+    body {
+        background-image: url("<?php echo validate_image($_settings->info('cover')) ?>");
+        background-size: cover;
+        background-repeat: no-repeat;
     }
-    .login-title{
-      text-shadow: 2px 2px black
+
+    .login-title {
+        text-shadow: 2px 2px black
     }
+
     /* #login{
       flex-direction:column !important
     } */
-    #login{
-        direction:rtl
+    #login {
+        direction: rtl
     }
-    #login > *{
-        direction:ltr
-    }
-    #logo-img{
-        height:150px;
-        width:150px;
-        object-fit:scale-down;
-        object-position:center center;
-        border-radius:100%;
-    }
-    #login .col-7,#login .col-5{
-      width: 100% !important;
-      max-width:unset !important
-    } 
-  </style>
-  </head>
 
- <?php require_once('inc/header.php') ?>
+    #login>* {
+        direction: ltr
+    }
+
+    #logo-img {
+        height: 150px;
+        width: 150px;
+        object-fit: scale-down;
+        object-position: center center;
+        border-radius: 100%;
+    }
+
+    #login .col-7,
+    #login .col-5 {
+        width: 100% !important;
+        max-width: unset !important
+    }
+    </style>
+</head>
+
+<?php require_once('inc/header.php') ?>
+
 <body class="hold-transition ">
-  <script>
+    <script>
     start_loader()
-  </script>
-  
-  
-  <?php if($_settings->chk_flashdata('success')): ?>
-      <script>
-        alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
-      </script>
-      <?php endif;?> 
-<div class="h-100 d-flex  align-items-center w-100" id="login">
-    <div class="col-7 h-100 d-flex align-items-center justify-content-center">
-      <div class="w-100 h-100">
-        <center><img src="<?= validate_image($_settings->info('logo')) ?>" alt="" id="logo-img"></center>
-        <h1 style="text-align:center;color:white;text-shadow: 2px 2px 8px #FF0000;"><b><?php echo $_settings->info('short_name') ?> - Etudiants</b></h1>
-      </div>
-      
-    </div>
-    <div class="col-5 h-100 bg-gradient bg-navy">
-        <div class="w-100 d-flex justify-content-center align-items-center h-100 text-navy">
-            <div class="card card-outline card-primary rounded-0 shadow col-lg-10 col-md-10 col-sm-5">
-                <div class="card-header">
-                    <h5 class="card-title text-center text-dark"><b>Connexion</b></h5>
-                </div>
-                <div class="card-body">
-                    <form action="" id="slogin-form" autocomplete="on">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="form-group">
-                                    <input type="email" name="email" id="email" placeholder="Email" class="form-control form-control-border" autocomplete="off" required>
+    </script>
+
+
+    <?php if($_settings->chk_flashdata('success')): ?>
+    <script>
+    alert_toast("<?php echo $_settings->flashdata('success') ?>", 'success')
+    </script>
+    <?php endif;?>
+    <div class="h-100 d-flex  align-items-center w-100" id="login">
+        <div class="col-7 h-100 d-flex align-items-center justify-content-center">
+            <div class="w-100 h-100">
+                <center><img src="<?= validate_image($_settings->info('logo')) ?>" alt="" id="logo-img"></center>
+                <h1 style="text-align:center;color:white;text-shadow: 2px 2px 8px #FF0000;">
+                    <b><?php echo $_settings->info('short_name') ?> - Etudiants</b>
+                </h1>
+            </div>
+
+        </div>
+        <div class="col-5 h-100 bg-danger bg-gradient">
+            <div class="w-100 d-flex justify-content-center align-items-center h-100 text-navy">
+                <div class="card card-outline card-primary rounded-0 shadow col-lg-10 col-md-10 col-sm-5">
+                    <div class="card-header">
+                        <h5 class="card-title text-center text-dark"><b>Connexion</b></h5>
+                    </div>
+                    <div class="card-body">
+                        <form action="" id="slogin-form">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <input type="email" name="email" id="email" placeholder="Email"
+                                            class="form-control form-control-border" autocomplete="off" required>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="form-group">
-                                    <input type="password" name="password" id="password" placeholder="Mot de passe" class="form-control form-control-border" autocomplete="off" required>
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <input type="password" name="password" id="password" placeholder="Mot de passe"
+                                            class="form-control form-control-border" autocomplete="off" required>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="row">
+                            <div class="row">
                                 <div class="col-lg-12 py-2">
                                     <button class="btn btn-success btn-block">Connexion</button>
                                 </div>
-                                <div class="col-lg-12"> <button class="btn btn-secondary btn-block"><a href="index.php"> Retour</a></button>
-                              </div>
-                        </div>
-                    </form>
+                                <div class="col-lg-12"> <button class="btn btn-secondary btn-block"><a href="index.php">
+                                            Retour</a></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
-<!-- Select2 -->
-<script src="<?php echo base_url ?>plugins/select2/js/select2.full.min.js"></script>
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+    <!-- Select2 -->
+    <script src="<?php echo base_url ?>plugins/select2/js/select2.full.min.js"></script>
 
-<script>
-  
-  $(document).ready(function(){
-    end_loader();
-    // Registration Form Submit
-    $('#slogin-form').submit(function(e){
-        e.preventDefault()
-        var _this = $(this)
+    <script>
+    $(document).ready(function() {
+        end_loader();
+        // Registration Form Submit
+        $('#slogin-form').submit(function(e) {
+            e.preventDefault()
+            var _this = $(this)
             $(".pop-msg").remove()
             $('#password, #cpassword').removeClass("is-invalid")
-        var el = $("<div>")
+            var el = $("<div>")
             el.addClass("alert pop-msg my-2")
             el.hide()
-        start_loader();
-        $.ajax({
-            url:_base_url_+"classes/Login.php?f=student_login",
-            method:'POST',
-            data:_this.serialize(),
-            dataType:'json',
-            error:err=>{
-                console.log(err)
-                el.text("An error occured while saving the data")
-                el.addClass("alert-danger")
-                _this.prepend(el)
-                el.show('slow')
-                end_loader();
-            },
-            success:function(resp){
-                if(resp.status == 'success'){
-                    location.href= "./"
-                }else if(!!resp.msg){
-                    el.text(resp.msg)
+            start_loader();
+            $.ajax({
+                url: _base_url_ + "classes/Login.php?f=student_login",
+                method: 'POST',
+                data: _this.serialize(),
+                dataType: 'json',
+                error: err => {
+                    console.log(err)
+                    el.text("An error occured while saving the data")
                     el.addClass("alert-danger")
                     _this.prepend(el)
-                    el.show('show')
-                }else{
-                    el.text("Une erreur s'est produite lors de l'enregistrement des données")
-                    el.addClass("alert-danger")
-                    _this.prepend(el)
-                    el.show('show')
+                    el.show('slow')
+                    end_loader();
+                },
+                success: function(resp) {
+                    if (resp.status == 'success') {
+                        location.href = "./"
+                    } else if (!!resp.msg) {
+                        el.text(resp.msg)
+                        el.addClass("alert-danger")
+                        _this.prepend(el)
+                        el.show('show')
+                    } else {
+                        el.text(
+                            "Une erreur s'est produite lors de l'enregistrement des données"
+                        )
+                        el.addClass("alert-danger")
+                        _this.prepend(el)
+                        el.show('show')
+                    }
+                    end_loader();
+                    $('html, body').animate({
+                        scrollTop: 0
+                    }, 'fast')
                 }
-                end_loader();
-                $('html, body').animate({scrollTop: 0},'fast')
-            }
+            })
         })
     })
-  })
-</script>
+    </script>
 </body>
+
 </html>
